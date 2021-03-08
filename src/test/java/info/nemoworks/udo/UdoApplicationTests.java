1170e81ace15df88b2c61ad1f71b3231e5e1bdfc
Chun <caochun@gmail.com>
2021-03-08 15:07:06 +0800

    spring start

---
+++ src/test/java/info/nemoworks/udo/UdoApplicationTests.java
@@ -0,0 +1,13 @@
+package info.nemoworks.udo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
