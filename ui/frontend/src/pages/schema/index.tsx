a611651080594353cd1bfde7732e21b379631324
PerishCode <perishcode@gmail.com>
2021-03-12 16:00:33 +0800

    some trick to fix formdata bug

--- ui/frontend/src/pages/schema/index.tsx
+++ ui/frontend/src/pages/schema/index.tsx
@@ -5,12 +5,63 @@ import { composer, extractor, transformer } from '@/utils/XForm'
 import './index.sass'
 
 export default function Page() {
+  const [initialFormData] = useState({
+    Name: '一个房间',
+    Area: 15,
+    Size: {
+      length: 10,
+      width: 8,
+      height: 15,
+    },
+    DeviceList: ['净化器', '小太阳'],
+  })
   const [formData, setFormData] = useState(null)
   const [schema, setSchema] = useState({
-    type: 'array',
-    template: {
-      type: 'string',
-      title: 'item',
+    type: 'object',
+    title: '房间信息',
+    properties: {
+      Name: {
+        type: 'string',
+        title: '房间名',
+        description: '房间名',
+      },
+      Area: {
+        type: 'number',
+        title: '房间面积',
+        description: '房间面积(平方米)',
+      },
+      Size: {
+        type: 'object',
+        properties: {
+          length: {
+            type: 'number',
+            title: '长',
+            description: '长',
+          },
+          width: {
+            type: 'number',
+            title: '宽',
+            description: '宽',
+          },
+          height: {
+            type: 'number',
+            title: '高',
+            description: '高',
+          },
+        },
+      },
+      DeviceList: {
+        type: 'array',
+        template: {
+          // type: "link",
+          // url: "/schemas",
+          // params: {
+          //     kind: "device",
+          // },
+          type: 'string',
+          title: 'Device',
+        },
+      },
     },
   })
 
@@ -30,8 +81,8 @@ export default function Page() {
       <div className="preview-form">
         <XForm
           schema={schema}
-          onChange={console.log}
-          formData={['123', '456']}
+          formData={initialFormData}
+          onChange={setFormData}
           transformer={transformer}
           extractor={extractor}
           composer={composer}
