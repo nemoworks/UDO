48a85cf4a092052b927fe4deb626b245a14ca182
PerishCode <perishcode@gmail.com>
2021-03-17 15:58:48 +0800

    nothing

--- ui/frontend/src/components/XForm/examples/link.ts
+++ ui/frontend/src/components/XForm/examples/link.ts
@@ -8,7 +8,6 @@ const initialSchema = {
     title: 'Link 类型一维数组',
     template: {
       type: 'link',
-      title: '设备',
       url: '/api/object',
     },
   },
---
+++ ui/frontend/src/components/XForm/renders/Tooltip.tsx
@@ -0,0 +1,3 @@
+export default function Tooltip({ schema, children }) {
+  return <div className="Tooltip container">{children}</div>
+}
