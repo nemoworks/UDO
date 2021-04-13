b7e1911ac7f2f814fd79a09d35e840b367aadff1
TcPaulyue <1195882464@qq.com>
2021-04-13 18:51:47 +0800

    add mqtt

---
+++ mqttClient/src/main/java/info/nemoworks/udo/Subscriber.java
@@ -0,0 +1,21 @@
+package info.nemoworks.udo;
+
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class Subscriber {
+
+    public static void main(String[] args) throws MqttException {
+
+        System.out.println("== START SUBSCRIBER ==");
+
+        MqttClient client=new MqttClient("tcp://localhost:1883", MqttClient.generateClientId());
+        client.setCallback( new SimpleMqttCallBack() );
+        client.connect();
+
+        client.subscribe("iot_data");
+
+    }
+
+}
