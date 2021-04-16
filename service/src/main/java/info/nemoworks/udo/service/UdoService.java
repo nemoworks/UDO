cc2885f5c7d5e5b2adc919b4533dc8470369eb6d
NovelistChan <774527381@qq.com>
2021-04-16 18:51:57 +0800

    slight refactor

--- service/src/main/java/info/nemoworks/udo/service/UdoService.java
+++ service/src/main/java/info/nemoworks/udo/service/UdoService.java
@@ -37,26 +37,26 @@ public class UdoService {
         return udoRepository.saveUdo(doc, doc.getSchemaId());
     }
 
-    public Udo findUdoById(String udoi, String collection) throws UdoPersistException, TablePersistException {
-//        collection = collection.substring(0, 1).toLowerCase() + collection.substring(1);
-        Udo doc = udoRepository.findUdo(udoi, collection);
+    public Udo findUdoById(String udoi, String schemaId) throws UdoPersistException, TablePersistException {
+//        schemaId = schemaId.substring(0, 1).toLowerCase() + schemaId.substring(1);
+        Udo doc = udoRepository.findUdo(udoi, schemaId);
         if (doc == null) {
-            throw new UdoPersistException("Doc " + udoi + " does not exist.\n" + collection + udoi);
+            throw new UdoPersistException("Doc " + udoi + " does not exist.\n" + schemaId + udoi);
         }
         return doc;
     }
 
-    public List<Udo> findAllUdos(String collection) {
-        return udoRepository.findAllUdos(collection);
+    public List<Udo> findAllUdos(String schemaId) {
+        return udoRepository.findAllUdos(schemaId);
     }
 
-    public List<Udo> deleteUdoById(String udoi, String collection) throws UdoPersistException, TablePersistException {
-        Udo doc = udoRepository.findUdo(udoi, collection);
+    public List<Udo> deleteUdoById(String udoi, String schemaId) throws UdoPersistException, TablePersistException {
+        Udo doc = udoRepository.findUdo(udoi, schemaId);
         if (doc == null) {
             throw new UdoPersistException("Doc " + udoi + " does not exist.");
         }
-        udoRepository.deleteUdo(udoi, collection);
-        return udoRepository.findAllUdos(collection);
+        udoRepository.deleteUdo(udoi, schemaId);
+        return udoRepository.findAllUdos(schemaId);
     }
 
     public Udo updateUdo(Udo udo, String udoi) throws UdoPersistException, TablePersistException {
